60f2a45cb18c585d78892d4a344ec515d9b8fc8d
Ajay MT <ajay.tatachar@gmail.com>
2014-10-25 12:16:37 +0530

    free things

--- test.c
+++ test.c
@@ -18,19 +18,23 @@ int main (int argc, char *argv[])
 
     char *world = dict_get(d, "hello");
     printf("    getting 'hello': %s\n", world);
+    free(world);
 
     char *bar = dict_get(d, "foo");
     printf("    getting 'foo': %d\n", atoi(bar));
+    free(bar);
 
     printf("\n  setting things...\n");
 
     dict_set(d, "hello", "people");
     char *people = dict_get(d, "hello");
     printf("    setting 'hello': %s\n", people);
+    free(people);
 
     dict_set(d, "baz", "quux");
     char *quux = dict_get(d, "baz");
     printf("    setting 'baz': %s\n", quux);
+    free(quux);
 
     printf("\n  dict length: %i\n", dict_len(d));
 
@@ -41,6 +45,7 @@ int main (int argc, char *argv[])
     for (int i = 0; i < dict_len(d); i++)
         printf(" %s", keys[i]);
     printf("\n");
+    free(keys);
 
     char **values = dict_values(d);
     printf("    values:");
@@ -48,6 +53,7 @@ int main (int argc, char *argv[])
         if (i == 1) printf(" %d", atoi(values[i]));
         else printf(" %s", values[i]);
     printf("\n");
+    free(values);
 
     printf("\n  checking for keys...\n");
 
@@ -66,6 +72,8 @@ int main (int argc, char *argv[])
     dict_del(d, "foo");
     newkeys = dict_keys(d);
     printf("    deleting key 'foo': %s %s\n", oldkeys[0], newkeys[0]);
+    free(oldkeys);
+    free(newkeys);
 
     printf("\n  freeing dict...\n");
     dict_free(d);
